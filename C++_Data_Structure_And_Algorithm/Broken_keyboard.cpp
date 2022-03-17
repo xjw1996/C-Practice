d7b6b2aec1c764d1972f0f0f5da3d92647b15dfa
xjw1996 <50350039+xjw1996@users.noreply.github.com>
2022-03-17 13:11:18 +0900

    Update Broken_keyboard.cpp

--- C++_Data_Structure_And_Algorithm/Broken_keyboard.cpp
+++ C++_Data_Structure_And_Algorithm/Broken_keyboard.cpp
@@ -7,14 +7,15 @@ class broken_keyboard {
 public:
     void input();
     void solve(std::string s);
+    void print() { std::cout << "I am s_" << s_ << std::endl; }
 private:
     std::string s_;
-    std::list<char> test;
-    std::list<char>::iterator arr_ptr = test.begin();// 这道题的指针需要初始化
 };
 
 void broken_keyboard::solve(std::string s)
 {
+    std::list<char> test;
+    std::list<char>::iterator arr_ptr = test.begin();// 这道题的指针需要初始化
     int len = s.size();
     for (int i = 0;i < len;i++) {
         if (s[i] =='[') {// <-- Single quotes here.双引号是 C++ 中 c 字符串的快捷语法。如果要比较单个字符，则必须改用单引号
@@ -25,21 +26,20 @@ void broken_keyboard::solve(std::string s)
         }
         if (s[i] != '[' && s[i] != ']') {
             test.insert(arr_ptr, s[i]);
-            //arr_ptr++;
         }
     }
     for (arr_ptr = test.begin();arr_ptr != test.end();arr_ptr++) 
         std::cout << *arr_ptr;
-    //std::cout << s << std::endl;
-    this->s_.clear();
-    //this->s_ = "";
-    //std::cout << s << std::endl;
+    std::cout <<  "------------------------" << std::endl;
 }
 
 void broken_keyboard::input()
 {
     while (std::cin >> s_) {
         solve(s_);
+        //std::cin.clear();
+        this->s_.clear();
+        print();
       /*  arr_ptr = test.begin();*/
     }
 }
